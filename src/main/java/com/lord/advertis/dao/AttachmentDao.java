3f3bf655f935e10449021b80cdbce403b33f4901
wangsc <1334996110@qq.com>
2017-10-27 16:03:02 +0800

    添加findAll接口

--- src/main/java/com/lord/advertis/dao/AttachmentDao.java
+++ src/main/java/com/lord/advertis/dao/AttachmentDao.java
@@ -2,7 +2,12 @@ package com.lord.advertis.dao;
 
 import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Attachment;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface AttachmentDao extends BaseMapper<Attachment, Integer>{
 
+    public List<Attachment> findAll();
 }
