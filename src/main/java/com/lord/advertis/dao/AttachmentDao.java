281d3f5f7499113a3660109085d9093bacc91912
wangsc <1334996110@qq.com>
2017-10-27 12:30:46 +0800

    新增附件接口

---
+++ src/main/java/com/lord/advertis/dao/AttachmentDao.java
@@ -0,0 +1,8 @@
+package com.lord.advertis.dao;
+
+import com.lord.advertis.base.BaseMapper;
+import com.lord.advertis.domain.Attachment;
+
+public interface AttachmentDao extends BaseMapper<Attachment, Long>{
+
+}
