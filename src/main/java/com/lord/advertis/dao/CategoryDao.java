f4f44570d1537c3be7da9fb65ec45e7212e3667d
wangsc <1334996110@qq.com>
2017-10-27 12:31:46 +0800

    新增分类信息持久层

---
+++ src/main/java/com/lord/advertis/dao/CategoryDao.java
@@ -0,0 +1,8 @@
+package com.lord.advertis.dao;
+
+import com.lord.advertis.base.BaseMapper;
+import com.lord.advertis.domain.Category;
+
+public interface CategoryDao extends BaseMapper<Category, Long>{
+
+}
