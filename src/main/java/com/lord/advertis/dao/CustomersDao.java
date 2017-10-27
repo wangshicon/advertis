593eabab1730835533debf3fcb0a1a6ce4458099
wangsc <1334996110@qq.com>
2017-10-27 12:32:51 +0800

    新增客户意向持久层

---
+++ src/main/java/com/lord/advertis/dao/CustomersDao.java
@@ -0,0 +1,8 @@
+package com.lord.advertis.dao;
+
+import com.lord.advertis.base.BaseMapper;
+import com.lord.advertis.domain.Customers;
+
+public interface CustomersDao extends BaseMapper<Customers, Long>{
+
+}
