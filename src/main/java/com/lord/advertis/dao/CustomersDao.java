00a38753f843daeea514cede2bdf1da504d91f2d
wangsc <1334996110@qq.com>
2017-10-27 15:15:19 +0800

    修改主键泛型为Int类型

--- src/main/java/com/lord/advertis/dao/CustomersDao.java
+++ src/main/java/com/lord/advertis/dao/CustomersDao.java
@@ -3,6 +3,6 @@ package com.lord.advertis.dao;
 import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Customers;
 
-public interface CustomersDao extends BaseMapper<Customers, Long>{
+public interface CustomersDao extends BaseMapper<Customers, Integer>{
 
 }
