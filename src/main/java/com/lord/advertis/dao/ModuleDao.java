757d78fcbc05d9197b2d33d977da10616b0d42d3
wangsc <1334996110@qq.com>
2017-10-27 16:08:02 +0800

    添加findAll接口

--- src/main/java/com/lord/advertis/dao/ModuleDao.java
+++ src/main/java/com/lord/advertis/dao/ModuleDao.java
@@ -2,7 +2,13 @@ package com.lord.advertis.dao;
 
 import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Module;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ModuleDao extends BaseMapper<Module, Integer>{
+
+    public List<Module> findAll();
 
 }
