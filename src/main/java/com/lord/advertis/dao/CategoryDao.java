19e7a4859526ae89e31a8e05c4c7857100d5d9be
wangsc <1334996110@qq.com>
2017-10-27 16:03:50 +0800

    添加findAll接口

--- src/main/java/com/lord/advertis/dao/CategoryDao.java
+++ src/main/java/com/lord/advertis/dao/CategoryDao.java
@@ -3,6 +3,12 @@ package com.lord.advertis.dao;
 import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Category;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface CategoryDao extends BaseMapper<Category, Integer>{
 
+    public List<Category> findAll();
 }
