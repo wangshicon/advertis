d5e7e7a7f95eb3989ec03933976879b53009ebfe
wangsc <1334996110@qq.com>
2017-10-27 12:33:21 +0800

    新增页面模块持久层

---
+++ src/main/java/com/lord/advertis/dao/ModuleDao.java
@@ -0,0 +1,8 @@
+package com.lord.advertis.dao;
+
+import com.lord.advertis.base.BaseMapper;
+import com.lord.advertis.domain.Module;
+
+public interface ModuleDao extends BaseMapper<Module, Long>{
+
+}
