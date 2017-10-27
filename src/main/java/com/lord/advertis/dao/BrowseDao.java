def7b73ce501152c57cf610b1c1540735399bc39
wangsc <1334996110@qq.com>
2017-10-27 16:09:17 +0800

    添加注解

--- src/main/java/com/lord/advertis/dao/BrowseDao.java
+++ src/main/java/com/lord/advertis/dao/BrowseDao.java
@@ -2,7 +2,9 @@ package com.lord.advertis.dao;
 
 import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Browse;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BrowseDao extends BaseMapper<Browse, Integer>{
 
 }
