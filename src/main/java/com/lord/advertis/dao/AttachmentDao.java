3e711f4e6692f3949e28d6e2c6cf65cb19c08d2f
wangsc <1334996110@qq.com>
2017-10-27 15:13:08 +0800

    修改主键泛型为Int类型

--- src/main/java/com/lord/advertis/dao/AttachmentDao.java
+++ src/main/java/com/lord/advertis/dao/AttachmentDao.java
@@ -3,6 +3,6 @@ package com.lord.advertis.dao;
 import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Attachment;
 
-public interface AttachmentDao extends BaseMapper<Attachment, Long>{
+public interface AttachmentDao extends BaseMapper<Attachment, Integer>{
 
 }
