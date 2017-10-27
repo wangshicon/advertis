a0366188776ffe5d435db1f542b5d396d355c7d4
wangsc <1334996110@qq.com>
2017-10-27 15:14:50 +0800

    修改主键泛型为Int类型

--- src/main/java/com/lord/advertis/dao/CategoryDao.java
+++ src/main/java/com/lord/advertis/dao/CategoryDao.java
@@ -3,6 +3,6 @@ package com.lord.advertis.dao;
 import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Category;
 
-public interface CategoryDao extends BaseMapper<Category, Long>{
+public interface CategoryDao extends BaseMapper<Category, Integer>{
 
 }
