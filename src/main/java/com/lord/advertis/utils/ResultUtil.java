f5c1d868a370a1e37908987c96733dbb4f1ed9ad
wangsc <1334996110@qq.com>
2017-10-27 10:11:50 +0800

    异常捕获

---
+++ src/main/java/com/lord/advertis/utils/ResultUtil.java
@@ -0,0 +1,35 @@
+package com.lord.advertis.utils;
+
+import com.lord.advertis.common.ResultAjax;
+
+/**
+ * 
+ * @Description: 请求结果
+ * @author Lord
+ * @date 2017年5月22日 下午5:40:29
+ */
+public class ResultUtil {
+	
+	@SuppressWarnings("rawtypes")
+	public static ResultAjax success(Object object){
+		ResultAjax<Object> result = new ResultAjax<Object>();
+		result.setCode(0);
+		result.setMsg("成功");
+		result.setData(object);
+		return result;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static ResultAjax success(){
+		return success(null);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static ResultAjax error(Integer code, String msg){
+		ResultAjax<Object> result = new ResultAjax<Object>();
+		result.setCode(code);
+		result.setMsg(msg);
+		return result;
+	}
+
+}
