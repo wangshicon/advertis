2cdb508e9293d1b1aa0cb7d1dd8194ee62cf4222
wangsc <1334996110@qq.com>
2017-10-27 17:01:53 +0800

    客户意向业务实现逻辑类

---
+++ src/main/java/com/lord/advertis/service/serviceimpl/CustomersServiceImpl.java
@@ -0,0 +1,51 @@
+package com.lord.advertis.service.serviceimpl;
+
+import com.lord.advertis.dao.CategoryDao;
+import com.lord.advertis.dao.CustomersDao;
+import com.lord.advertis.domain.Category;
+import com.lord.advertis.domain.Customers;
+import com.lord.advertis.service.CategoryService;
+import com.lord.advertis.service.CustomersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 
+ * @Description: 客户意向业务实现逻辑类
+ * @author Lord
+ * @date 2017年10月27日 下午3:06:31
+ */
+@Service
+public class CustomersServiceImpl implements CustomersService {
+
+	@Autowired
+	private CustomersDao customersDao;
+
+	@Override
+	public int insert(Customers customers) {
+		return customersDao.insertSelective(customers);
+	}
+
+	@Override
+	public int delete(Integer id) {
+		return customersDao.deleteByPrimaryKey(id);
+	}
+
+	@Override
+	public int update(Customers customers) {
+		return customersDao.updateByPrimaryKeySelective(customers);
+	}
+
+	@Override
+	public Customers findById(Integer id) {
+		return customersDao.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public List<Customers> findAll() {
+		return customersDao.findAll();
+	}
+
+}
