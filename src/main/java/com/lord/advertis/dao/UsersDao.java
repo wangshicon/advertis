518e98aeef0351e0cd5d492cb2da02ef17ae102e
wangsc <1334996110@qq.com>
2017-10-27 16:09:42 +0800

    添加注解

--- src/main/java/com/lord/advertis/dao/UsersDao.java
+++ src/main/java/com/lord/advertis/dao/UsersDao.java
@@ -2,7 +2,9 @@ package com.lord.advertis.dao;
 
 import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Users;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsersDao extends BaseMapper<Users, Integer>{
 
 }
