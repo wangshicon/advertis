e750c55402b8fd29f36857614d924f962fb6ca37
wangsc <1334996110@qq.com>
2017-10-27 09:46:15 +0800

    配置mpper扫描dao

--- src/main/java/com/lord/advertis/AdvertisApplication.java
+++ src/main/java/com/lord/advertis/AdvertisApplication.java
@@ -1,9 +1,11 @@
 package com.lord.advertis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lord.advertis.dao")
 public class AdvertisApplication {
 
 	public static void main(String[] args) {
