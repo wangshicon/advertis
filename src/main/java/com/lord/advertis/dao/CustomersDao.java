41057e8aceb035cec709cf476990243969b5a967
wangsc <1334996110@qq.com>
2017-10-27 16:05:16 +0800

    添加findAll接口

--- src/main/java/com/lord/advertis/dao/CustomersDao.java
+++ src/main/java/com/lord/advertis/dao/CustomersDao.java
@@ -2,7 +2,13 @@ package com.lord.advertis.dao;
 
 import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Customers;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CustomersDao extends BaseMapper<Customers, Integer>{
+
+    public List<Customers> findAll();
 
 }
