41c71e759e32663a5cae76b2d63c9bdbf154c3cd
wangsc <1334996110@qq.com>
2017-10-27 12:31:23 +0800

    新增浏览量持久层

---
+++ src/main/java/com/lord/advertis/dao/BrowseDao.java
@@ -0,0 +1,8 @@
+package com.lord.advertis.dao;
+
+import com.lord.advertis.base.BaseMapper;
+import com.lord.advertis.domain.Browse;
+
+public interface BrowseDao extends BaseMapper<Browse, Long>{
+
+}
