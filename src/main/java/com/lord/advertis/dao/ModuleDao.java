ab97d5753232d2c14f3f2be6e979308d6e1e5847
wangsc <1334996110@qq.com>
2017-10-27 15:15:51 +0800

    修改主键泛型为Int类型

--- src/main/java/com/lord/advertis/dao/ModuleDao.java
+++ src/main/java/com/lord/advertis/dao/ModuleDao.java
@@ -3,6 +3,6 @@ package com.lord.advertis.dao;
 import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Module;
 
-public interface ModuleDao extends BaseMapper<Module, Long>{
+public interface ModuleDao extends BaseMapper<Module, Integer>{
 
 }
