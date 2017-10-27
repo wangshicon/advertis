13ccf0c3f20ea237ac8bbd81827518a928964e45
wangsc <1334996110@qq.com>
2017-10-27 12:33:45 +0800

    新增用户信息持久层

---
+++ src/main/java/com/lord/advertis/dao/UsersDao.java
@@ -0,0 +1,8 @@
+package com.lord.advertis.dao;
+
+import com.lord.advertis.base.BaseMapper;
+import com.lord.advertis.domain.Users;
+
+public interface UsersDao extends BaseMapper<Users, Long>{
+
+}
