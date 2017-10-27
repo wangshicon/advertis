f1c4275a59aa89c921d8a8f2b28c75c616cb9cb2
wangsc <1334996110@qq.com>
2017-10-27 15:13:33 +0800

    修改主键泛型为Int类型

--- src/main/java/com/lord/advertis/dao/BrowseDao.java
+++ src/main/java/com/lord/advertis/dao/BrowseDao.java
@@ -3,6 +3,6 @@ package com.lord.advertis.dao;
 import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Browse;
 
-public interface BrowseDao extends BaseMapper<Browse, Long>{
+public interface BrowseDao extends BaseMapper<Browse, Integer>{
 
 }
