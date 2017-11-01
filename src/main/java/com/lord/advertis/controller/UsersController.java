f583d7b2a73aaf5d57dcaadded09a9bea3d0f591
wangsc <1334996110@qq.com>
2017-11-01 11:52:42 +0800

    用户管理控制层基本完成

---
+++ src/main/java/com/lord/advertis/controller/UsersController.java
@@ -0,0 +1,61 @@
+package com.lord.advertis.controller;
+
+import com.lord.advertis.common.ResultAjax;
+import com.lord.advertis.domain.Users;
+import com.lord.advertis.interceptor.WebSecurityConfig;
+import com.lord.advertis.service.UsersService;
+import com.lord.advertis.utils.ResultUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * 用户管理
+ * Created by on 2017/10/31.
+ */
+
+@Controller
+@RequestMapping(value = "/user")
+public class UsersController {
+
+    @Autowired
+    private UsersService usersService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    @ResponseBody
+    public ResultAjax<Users> loginPost(Users users, BindingResult bindingResult, HttpSession session) {
+        if(bindingResult.hasErrors()){
+            return ResultUtil.error(1, bindingResult.getFieldError().getDefaultMessage());
+        }
+        Users user = usersService.findByLoginName(users.getLoginname());
+
+        if (!users.getPassword().equals(user.getPassword())) {
+            return ResultUtil.error(2, "账户密码不匹配~");
+        }
+
+        // 设置session
+        session.setAttribute(WebSecurityConfig.SESSION_KEY, user.getLoginname());
+
+        return ResultUtil.success();
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        // 移除session
+        session.removeAttribute(WebSecurityConfig.SESSION_KEY);
+        return "redirect:/login";
+    }
+
+}
