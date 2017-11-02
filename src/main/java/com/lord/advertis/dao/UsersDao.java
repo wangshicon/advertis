feb3ceca08f401dea84fb9042448c4aaca7738c7
wangsc <1334996110@qq.com>
2017-11-02 12:30:10 +0800

    添加查询所有方法

--- src/main/java/com/lord/advertis/dao/UsersDao.java
+++ src/main/java/com/lord/advertis/dao/UsersDao.java
@@ -4,8 +4,12 @@ import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Users;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsersDao extends BaseMapper<Users, Integer>{
+
+    public List<Users> findAll();
 
     public Users findByLoginName(String loginname);
 
