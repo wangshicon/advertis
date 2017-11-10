6db1d0b2f83d98d814c9cf0cdcd65c450bbce2c2
wangsc <1334996110@qq.com>
2017-11-10 11:51:06 +0800

    更改bootstrap 模态框垂直居中方法

--- src/main/resources/static/controller/config.js
+++ src/main/resources/static/controller/config.js
@@ -11,11 +11,20 @@ function Refresh() {
 };
 
 //模态框垂直和水平居中
-$('#myModal').on('shown.bs.modal', function (e) {  
-    // 关键代码，如没将modal设置为 block，则$modala_dialog.height() 为零  
-    $(this).css('display', 'block');  
-    var modalHeight=$(window).height() / 3 - $('#myModal .modal-dialog').height() / 2;  
-    $(this).find('.modal-dialog').css({  
-        'margin-top': modalHeight  
-    });  
+$(function(){
+    // dom加载完毕
+    var $m_btn = $('#modalBtn');
+    var $modal = $('#myModal');
+    $m_btn.on('click', function(){
+        $modal.modal({backdrop: 'static'});
+    });
+
+    // 测试 bootstrap 居中
+    $modal.on('show.bs.modal', function(){
+        var $this = $(this);
+        var $modal_dialog = $this.find('.modal-dialog');
+        // 关键代码，如没将modal设置为 block，则$modala_dialog.height() 为零
+        $this.css('display', 'block');
+        $modal_dialog.css({'margin-top': Math.max(0, ($(window).height() - $modal_dialog.height()) / 2) });
+    });
 });
