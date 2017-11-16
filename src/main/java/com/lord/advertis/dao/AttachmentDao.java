1a7d23eeabb6a9f0f24ede346de7f2cf01b75a8f
wangsc <1334996110@qq.com>
2017-11-16 11:22:11 +0800

    添加查询部分数据的方法

--- src/main/java/com/lord/advertis/dao/AttachmentDao.java
+++ src/main/java/com/lord/advertis/dao/AttachmentDao.java
@@ -2,6 +2,7 @@ package com.lord.advertis.dao;
 
 import com.lord.advertis.base.BaseMapper;
 import com.lord.advertis.domain.Attachment;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,6 @@ public interface AttachmentDao extends BaseMapper<Attachment, Integer>{
     public List<Attachment> findAll();
 
     public List<Attachment> findByCategoryId(Integer cid);
+
+    public List<Attachment> selectBylimit(@Param("offset")Integer offset, @Param("limit")Integer limit);
 }
