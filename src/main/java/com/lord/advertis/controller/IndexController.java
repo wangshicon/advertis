257861c30d8f63fede4af3a07da2fda45422712b
wangsc <1334996110@qq.com>
2017-11-17 11:23:22 +0800

    首页返回数据调整，增加session

--- src/main/java/com/lord/advertis/controller/IndexController.java
+++ src/main/java/com/lord/advertis/controller/IndexController.java
@@ -7,11 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
+
+import static com.lord.advertis.consts.Brows.BROWS_AMOUNT;
 
 /**
  *
- * 仪表盘数据
+ * 主页面视图控制器
  * Created by on 2017/11/1.
  */
 @Controller
@@ -32,8 +35,16 @@ public class IndexController {
     @Autowired
     private AttachmentService attachmentService;
 
+    /**
+     *
+     * @Title: index
+     * @Description: 前台首页控制器
+     * @return
+     * @author Lord
+     * @date 2017年11月17日 上午11:01:23
+     */
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, HttpSession session) {
         List<Module> moduleList = moduleService.findAll();
         List<Category> categoryList = categoryService.findAll();
         List<Attachment> attachmentList = attachmentService.selectByLimit(0, 8);
@@ -44,9 +55,20 @@ public class IndexController {
         model.addAttribute("contact", moduleList.get(4));
         model.addAttribute("categorys", categoryList);
         model.addAttribute("attachments", attachmentList);
+        model.addAttribute("todayNum", BROWS_AMOUNT);
+        session.setAttribute("isNew", session.isNew());
         return "index";
     }
 
+
+    /**
+     *
+     * @Title: manager
+     * @Description: 后台管理仪表盘控制器
+     * @return
+     * @author Lord
+     * @date 2017年11月17日 上午11:04:23
+     */
     @GetMapping("/manager")
     public String manager(Model model){
         List<Customers> customersList = customersService.selectByLimit(0,4);
