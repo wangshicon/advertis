7d88d56799ca887621188cf09c56f0904e0c0743
wangsc <1334996110@qq.com>
2017-11-17 11:21:46 +0800

    增加浏览量常量BROWS_AMOUNT

---
+++ src/main/java/com/lord/advertis/consts/Brows.java
@@ -0,0 +1,12 @@
+package com.lord.advertis.consts;
+
+/**
+ * @Description: 页面浏览量全局变量
+ * @author Lord
+ * @date 2017年11月17日 上午9:14:59
+ */
+public class Brows {
+
+    // 浏览量
+    public static Integer BROWS_AMOUNT = 0;
+}
