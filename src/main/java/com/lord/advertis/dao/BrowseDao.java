edfd4d843b53a92cde3bfd810f7ddeced59800e4
wangsc <1334996110@qq.com>
2017-11-25 09:44:26 +0800

    查询一周的浏览量

--- src/main/java/com/lord/advertis/dao/BrowseDao.java
+++ src/main/java/com/lord/advertis/dao/BrowseDao.java
@@ -1,10 +1,15 @@
 package com.lord.advertis.dao;
 
 import com.lord.advertis.base.BaseMapper;
+import com.lord.advertis.domain.Attachment;
 import com.lord.advertis.domain.Browse;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BrowseDao extends BaseMapper<Browse, Integer>{
+
+    public List<Browse> findWeekCount();
 
 }
